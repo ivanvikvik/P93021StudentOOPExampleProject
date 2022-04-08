6530106f00416307048c07419f8552efb4399a8e
Victor <ivanvikvik@gmail.com>
2022-04-08 18:59:47 +0300

    Add Group implementation

---
+++ Group.cpp
@@ -0,0 +1,81 @@
+#include "Group.h"
+
+Group::Group(){
+	name = "no group name";
+	size = 0;
+	list = NULL;
+}
+
+Group::Group(string name){
+	this->name = name;
+	size = 0;
+	list = NULL;
+}
+
+Group::Group(string name, Student* list, int size){
+	this->name = name;
+	this->size = size;
+	this->list = list;
+}
+
+Group::~Group(){}
+
+string Group::getName(){
+	return name;
+}
+
+void Group::setName(string name){
+	this->name = name;
+}
+
+int Group::getSize(){
+	return size;
+}
+
+Student Group::get(int index){
+	if (list == NULL || index < 0 || index >= size) {
+		return Student("", 0, 0);
+	}
+	else {
+		return list[index];
+	}
+}
+
+void Group::set(int index, Student student){
+	if (list != NULL && index >= 0 && index < size) {
+		list[index] = student;
+	}
+}
+
+void Group::add(Student student){
+	if (list == NULL) {
+		list = new Student[1];
+		size = 1;
+		list[0] = student;
+	}
+	else {
+		Student* temp = new Student[size + 1];
+		for (int i = 0; i < size; i++)
+		{
+			temp[i] = list[i];
+		}
+		temp[size] = student;
+		size++;
+		delete[] list;
+		list = temp;
+	}
+}
+
+string Group::getInfo(){
+	if (list == NULL) {
+		return "Group " + name + " is empty.";
+	} else {
+		string msg = "Studens of group " + name + ":\n";
+		for (int i = 0; i < size; i++)
+		{
+			msg += list[i].getInfo() + "\n";
+		}
+
+		return msg;
+	}
+}
--- Main.cpp
+++ Main.cpp
@@ -1,13 +1,28 @@
 #include "Student.h"
+#include "Group.h"
 
 int main() {
+	Group group("10b");
+
 	Student st1("Alex", 14, 9);
 	Student st2("Nastya", 13, 8);
 	Student st3("Nikita", 15, 4);
 
-	cout << st1.getInfo() << endl;
-	cout << st2.getInfo() << endl;
-	cout << st3.getInfo() << endl;
+	group.add(st1);
+	group.add(st2);
+	group.add(st3);
 
+	double avg = 0;
+
+	for (int i = 0; i < group.getSize(); i++)
+	{
+		avg += group.get(i).getMark();
+	}
+
+	avg /= group.getSize();
+
+	cout << group.getInfo() << endl;
+	cout << "Avg mark of group " << group.getName() << " = " << avg << endl;
+	
 	return 0;
 }
