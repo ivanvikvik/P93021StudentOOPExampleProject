16bc8aa0bdce136405b0094e1c27cca5e20280d7
Victor <ivanvikvik@gmail.com>
2022-04-24 17:45:10 +0300

    Update Project

--- Main.cpp
+++ Main.cpp
@@ -1,21 +1,21 @@
-#include <ctime>
-#include "Student.h"
-#include "Group.h"
-#include "Manager.h"
-
-void changeMark(Student& st) {
-	srand(time(NULL));
-	// [rand() % (max - min + 1)] + min
-	st.mark = rand() % 7 + 4;	
-}
-
-int main() {
-	Student st2("Nastya", 13, 8, 'f');
-	
-	cout << "Before: " << st2.getInfo() << endl;
-	changeMark(st2);
-	cout << "After: " << st2.getInfo() << endl;
-
-
-	return 0;
-}
+//#include <ctime>
+//#include "Student.h"
+//#include "Group.h"
+//#include "Manager.h"
+//
+//void changeMark(Student& st) {
+//	srand(time(NULL));
+//	// [rand() % (max - min + 1)] + min
+//	st.mark = rand() % 7 + 4;	
+//}
+//
+//int main() {
+//	Student st2("Nastya", 13, 8, 'f');
+//	
+//	cout << "Before: " << st2.getInfo() << endl;
+//	changeMark(st2);
+//	cout << "After: " << st2.getInfo() << endl;
+//
+//
+//	return 0;
+//}
